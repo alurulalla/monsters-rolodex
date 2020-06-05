278ac53db04a3206dbe9a2ddd46be0f7df3ea10e
Aluru Murali Krishna <alurumuralikrishna@Alurus-MacBook-Pro.local>
2020-06-05 12:21:57 +0530

    Updates

---
+++ precache-manifest.1d7049f2c9aa95145cfb35cb11a022f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62a9ab1b0c5d91cc66417165798bb54d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5e153ba37c62275ee7a0",
+    "url": "/monsters-rolodex/static/css/main.8e014b52.chunk.css"
+  },
+  {
+    "revision": "86347b2f6489f803fae8",
+    "url": "/monsters-rolodex/static/js/2.1bad28ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.1bad28ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e153ba37c62275ee7a0",
+    "url": "/monsters-rolodex/static/js/main.46483804.chunk.js"
+  },
+  {
+    "revision": "f8e35ce8018b8294fac2",
+    "url": "/monsters-rolodex/static/js/runtime-main.5605acd4.js"
+  }
+]);
